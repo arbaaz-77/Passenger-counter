e1451325a3d897b80b446de31092a3b14e380572
Arbaaz Sumar <43441056+arbaaz-77@users.noreply.github.com>
2021-11-15 17:13:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
+
+count = 0;
+
+function increment() {
+    count++;
+    countEl.textContent = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
